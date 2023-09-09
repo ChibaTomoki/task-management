b87f4d2568ef7a60a1ba9c8edd5398b6f48cffa5
ChibaTomoki <michiamo.t1@gmail.com>
2023-09-09 23:15:41 +0900

    ナビゲーションタブの修正

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,48 +2,14 @@
 
 import MuiBox from '@mui/material/Box'
 import CssBaseline from '@mui/material/CssBaseline'
-import MuiTab from '@mui/material/Tab'
-import MuiTabs from '@mui/material/Tabs'
+import Tab from '@mui/material/Tab'
+import { default as Tabs } from '@mui/material/Tabs'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import useMediaQuery from '@mui/material/useMediaQuery'
 import { usePathname, useRouter } from 'next/navigation'
-import { MouseEvent, ReactNode, SyntheticEvent, useMemo } from 'react'
+import { ReactNode, SyntheticEvent, useMemo } from 'react'
 
-function samePageLinkNavigation(
-  event: MouseEvent<HTMLAnchorElement, MouseEvent>,
-) {
-  if (
-    event.defaultPrevented ||
-    event.button !== 0 || // ignore everything but left-click
-    event.metaKey ||
-    event.ctrlKey ||
-    event.altKey ||
-    event.shiftKey
-  ) {
-    return false
-  }
-  return true
-}
-
-interface LinkTabProps {
-  label?: string
-  href?: string
-}
-
-function LinkTab(props: LinkTabProps) {
-  return (
-    <MuiTab
-      component="a"
-      onClick={(event: MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-        // Routing libraries handle this, you can remove the onClick handle when using them.
-        if (samePageLinkNavigation(event)) {
-          event.preventDefault()
-        }
-      }}
-      {...props}
-    />
-  )
-}
+type TabLink = '/board' | '/table' | '/'
 
 export default function MainLayout({ children }: { children: ReactNode }) {
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
@@ -61,21 +27,8 @@ export default function MainLayout({ children }: { children: ReactNode }) {
   const router = useRouter()
   const pathname = usePathname()
 
-  const value = pathname === '/board' ? 0 : pathname === '/table' ? 1 : 2
-
-  const handleChange = (event: SyntheticEvent, newValue: number) => {
-    // event.type can be equal to focus with selectionFollowsFocus.
-    if (
-      event.type !== 'click' ||
-      (event.type === 'click' &&
-        samePageLinkNavigation(
-          event as MouseEvent<HTMLAnchorElement, MouseEvent>,
-        ))
-    ) {
-      if (newValue === 0) router.push('/board')
-      if (newValue === 1) router.push('/table')
-      if (newValue === 2) router.push('/')
-    }
+  const handleChange = (event: SyntheticEvent, newValue: TabLink) => {
+    router.push(newValue)
   }
 
   return (
@@ -83,18 +36,16 @@ export default function MainLayout({ children }: { children: ReactNode }) {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <MuiBox sx={{ width: '100%' }}>
-          <MuiTabs
-            value={value}
-            onChange={handleChange}
-            aria-label="nav tabs example"
-          >
-            <LinkTab label="Board" href="/board" />
-            <LinkTab label="Table" href="/table" />
-            <LinkTab label="Other" href="/" />
-          </MuiTabs>
+          <MuiBox sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <Tabs value={pathname} onChange={handleChange} aria-label="tabs">
+              <Tab label="Board" value="/board" />
+              <Tab label="Table" value="/table" />
+              <Tab label="Other" value="/" />
+            </Tabs>
+          </MuiBox>
         </MuiBox>
+        {children}
       </ThemeProvider>
-      {children}
     </div>
   )
 }
