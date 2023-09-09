739b9dae9c7331fca13a8a5e1d1804705a477c87
ChibaTomoki <michiamo.t1@gmail.com>
2023-09-09 16:36:40 +0900

    MUIのインストール

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,9 +1,11 @@
+import Button from '@mui/material/Button'
 import { ReactNode } from 'react'
 
 export default function MainLayout({ children }: { children: ReactNode }) {
   return (
     <div>
       <h1>Layout</h1>
+      <Button variant="contained">Hello world</Button>
       {children}
     </div>
   )
