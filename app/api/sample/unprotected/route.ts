c84306f255fdf57555c65799134e1b34a8c3f649
ChibaTomoki <michiamo.t1@gmail.com>
2023-09-11 01:08:18 +0900

    lint fixを実行

--- app/api/sample/unprotected/route.ts
+++ app/api/sample/unprotected/route.ts
@@ -1,5 +1,3 @@
 import { NextResponse } from 'next/server'
 
-export const GET = () => {
-  return NextResponse.json({ message: 'GET /api/sample/unprotected is called' })
-}
+export const GET = () => NextResponse.json({ message: 'GET /api/sample/unprotected is called' })
